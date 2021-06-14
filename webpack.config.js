5f61c36c6a0b4789d6f93d2a9e47348ba82abdec
David Gómez <38304163+DG-Develop@users.noreply.github.com>
2021-06-14 16:53:59 -0500

    mejorando la salida de archivos para produccion

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -60,6 +63,14 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: 'assets/[name]-[fullhash].css'
-        })
-    ]
+        }),
+        new CleanWebpackPlugin()
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ]
+    }
 }
